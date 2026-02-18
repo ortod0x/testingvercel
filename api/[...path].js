7f045a995067ac9512e5546966ae411b22f3ebd4
mimikatz.ps1 <131539265+ortod0x@users.noreply.github.com>
2026-02-18 21:55:44 +0700

    Create [...path].js

---
+++ api/[...path].js
@@ -0,0 +1,29 @@
+import httpProxy from "http-proxy";
+
+const target = "http://95.179.151.4:3000";
+
+const proxy = httpProxy.createProxyServer({
+  target,
+  changeOrigin: true,
+});
+
+export default function handler(req, res) {
+  return new Promise((resolve, reject) => {
+    proxy.web(req, res, {}, (err) => {
+      if (err) {
+        console.error("Proxy error:", err);
+        res.status(500).json({ error: "Proxy error" });
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
